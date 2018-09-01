b7da90ead90cdbba7aeec4d56767eb40222798f3
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2018-09-01 20:13:28 +0300

    Enabled chaining operators.

--- TSKit.Core/Sources/Chaining.swift
+++ TSKit.Core/Sources/Chaining.swift
@@ -12,45 +12,45 @@ infix operator =>? : ChainingPrecedence
 infix operator ==> : ChainingPrecedence
 infix operator ==>? : ChainingPrecedence
 
-func ==><A, B>(_ lhs: A, _ rhs: (A) throws -> B) rethrows -> B {
+public func ==><A, B>(_ lhs: A, _ rhs: (A) throws -> B) rethrows -> B {
     return try rhs(lhs)
 }
 
 
 @discardableResult
-func ==><A>(_ lhs: A, _ rhs: (A) throws -> Void) rethrows -> A {
+public func ==><A>(_ lhs: A, _ rhs: (A) throws -> Void) rethrows -> A {
     try rhs(lhs)
     return lhs
 }
 
 @discardableResult
-func ==><A>(_ lhs:  () throws -> A, _ rhs: (A) throws -> Void) rethrows -> A {
+public func ==><A>(_ lhs:  () throws -> A, _ rhs: (A) throws -> Void) rethrows -> A {
     let a = try lhs()
     try rhs(a)
     return a
 }
 
-func ==><A, B>(_ lhs: () throws -> A, _ rhs: (A) throws -> B) rethrows -> B {
+public func ==><A, B>(_ lhs: () throws -> A, _ rhs: (A) throws -> B) rethrows -> B {
     return try rhs(try lhs())
 }
 
-func ==>?<A, B>(_ lhs: A?, _ rhs: (A) throws -> B?) rethrows -> B? {
+public func ==>?<A, B>(_ lhs: A?, _ rhs: (A) throws -> B?) rethrows -> B? {
     return try lhs.flatMap(rhs)
 }
 
-func ==>?<A, B>(_ lhs: () throws -> A?, _ rhs: (A) throws -> B?) rethrows -> B? {
+public func ==>?<A, B>(_ lhs: () throws -> A?, _ rhs: (A) throws -> B?) rethrows -> B? {
     return try lhs().flatMap(rhs)
 }
 
-func =><A, B, C>(_ lhs: @escaping (A) -> B, _ rhs: @escaping (B) -> C) -> (A) -> C {
+public func =><A, B, C>(_ lhs: @escaping (A) -> B, _ rhs: @escaping (B) -> C) -> (A) -> C {
     return { rhs(lhs($0)) }
 }
 
-func =><A, B, C>(_ lhs: @escaping (A) -> B?, _ rhs: @escaping (B) -> C?) -> (A) -> C? {
+public func =><A, B, C>(_ lhs: @escaping (A) -> B?, _ rhs: @escaping (B) -> C?) -> (A) -> C? {
     return { lhs($0).flatMap(rhs) }
 }
 
-func =><A, B>(_ lhs: @escaping (A) -> B, _ rhs: @escaping (B) -> Void) -> (A) -> B {
+public func =><A, B>(_ lhs: @escaping (A) -> B, _ rhs: @escaping (B) -> Void) -> (A) -> B {
     return {
         let b = lhs($0)
         rhs(b)
@@ -58,7 +58,7 @@ func =><A, B>(_ lhs: @escaping (A) -> B, _ rhs: @escaping (B) -> Void) -> (A) ->
     }
 }
 
-func =>?<A, B>(_ lhs: @escaping (A) -> B?, _ rhs: @escaping (B) -> Void) -> (A) -> B? {
+public func =>?<A, B>(_ lhs: @escaping (A) -> B?, _ rhs: @escaping (B) -> Void) -> (A) -> B? {
     return {
         let b = lhs($0)
         b.flatMap(rhs)
@@ -66,7 +66,7 @@ func =>?<A, B>(_ lhs: @escaping (A) -> B?, _ rhs: @escaping (B) -> Void) -> (A) 
     }
 }
 
-func =>?<A>(_ lhs: @escaping () -> A?, _ rhs: @escaping (A) -> Void) -> () -> A? {
+public func =>?<A>(_ lhs: @escaping () -> A?, _ rhs: @escaping (A) -> Void) -> () -> A? {
     return {
         let a = lhs()
         a.flatMap(rhs)
