5b95784e75ae93d859af3b84abf7508821ed81e1
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-10-03 19:06:55 +0300

    Added method to extract relative urls.

---
+++ TSKit.Core/Sources/Extensions/URL/URL+Relative.swift
@@ -0,0 +1,21 @@
+/// - Since: 10/02/2019
+/// - Authors: Arkadii Hlushchevskyi
+/// - Copyright: © 2019. Arkadii Hlushchevskyi.
+/// - Seealsos: https://github.com/adya/TSKit.Core/blob/master/LICENSE.md
+
+public extension URL {
+    
+    /// Creates an `url` that is resolved relatively to specified `root`.
+    ///
+    /// The following example shows the effect of applying this method to an url.
+    ///
+    ///     let root = URL(fileURLWithPath: "my/root/")
+    ///     let absoluteFile = URL(fileURLWithPath: "my/root/somewhere/deep/down/file")
+    ///     let relativeFile = absoluteFile.relative(to: root)
+    ///     print(relativeFile)
+    ///     // Prints "somewhere/deep/down/file" and baseUrl = "my/root"
+    func relative(to root: URL) -> URL {
+        return URL(fileURLWithPath: path.replacingOccurrences(of: root.path, with: "").dropFirst() ==> String.init,
+                   relativeTo: root)
+    }
+}
