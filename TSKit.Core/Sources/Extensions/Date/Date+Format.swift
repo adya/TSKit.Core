2f087272253475497b7c79f868937029f6a54177
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-10-03 18:34:39 +0300

    Added support for calendars in Date extensions.

--- TSKit.Core/Sources/Extensions/Date/Date+Format.swift
+++ TSKit.Core/Sources/Extensions/Date/Date+Format.swift
@@ -9,9 +9,13 @@ import Foundation
 public extension Date {
     
     init?(from dateString: String,
-                 formattedWith targetFormat: String,
-                 in timezone: TimeZone = TimeZone.current) {
+          formattedWith targetFormat: String,
+          with calendarIdentifier: Calendar.Identifier? = nil,
+          in timezone: TimeZone = TimeZone.current) {
         let formatter = DateFormatter()
+        if let calendar = calendarIdentifier.flatMap(Calendar.init(identifier:)) {
+            formatter.calendar = calendar
+        }
         formatter.dateFormat = targetFormat
         formatter.timeZone = timezone
         guard let date = formatter.date(from: dateString) else { return nil }
@@ -20,7 +24,7 @@ public extension Date {
     }
     
     func toString(withFormat targetFormat : String,
-                         andTimezone timezone : TimeZone = TimeZone.current) -> String {
+                  andTimezone timezone : TimeZone = TimeZone.current) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = targetFormat
         formatter.timeZone = timezone
@@ -32,10 +36,14 @@ public extension Date {
 public extension String {
     
     init?(from date: Date,
-                 formattedWith dateFormat: String,
-                 in timezone: TimeZone = TimeZone.current) {
+          formattedWith dateFormat: String,
+          with calendarIdentifier: Calendar.Identifier? = nil,
+          in timezone: TimeZone = .current) {
         
         let formatter = DateFormatter()
+        if let calendar = calendarIdentifier.flatMap(Calendar.init(identifier:)) {
+            formatter.calendar = calendar
+        }
         formatter.dateFormat = dateFormat
         formatter.timeZone = timezone
         let stringDate = formatter.string(from: date)
@@ -45,9 +53,9 @@ public extension String {
     }
     
     init?(from date: Date,
-                 dateStyle: DateFormatter.Style = .short,
-                 timeStyle: DateFormatter.Style = .short,
-                 in timezone: TimeZone = .current) {
+          dateStyle: DateFormatter.Style = .short,
+          timeStyle: DateFormatter.Style = .short,
+          in timezone: TimeZone = .current) {
         let formatter = DateFormatter()
         formatter.dateStyle = dateStyle
         formatter.timeStyle = timeStyle
@@ -61,20 +69,22 @@ public extension String {
     
     @available(iOS 8.0, *)
     func reformattingDate(formattedWith sourceFormat: String,
-                                 `in` sourceTimezone: TimeZone = TimeZone.current,
-                                 toStringFormattedWith targetFormat: String,
-                                 `in` targetTimezone: TimeZone = TimeZone.current) -> String? {
-        if let date = Date(from: self, formattedWith: sourceFormat, in: sourceTimezone) {
-            return String(from: date, formattedWith: targetFormat, in: targetTimezone)
+                          with sourceCalendarIdentifier: Calendar.Identifier? = nil,
+                          `in` sourceTimezone: TimeZone = TimeZone.current,
+                          toStringFormattedWith targetFormat: String,
+                          with targetCalendarIdentifier: Calendar.Identifier? = nil,
+                          `in` targetTimezone: TimeZone = TimeZone.current) -> String? {
+        if let date = Date(from: self, formattedWith: sourceFormat, with: sourceCalendarIdentifier, in: sourceTimezone) {
+            return String(from: date, formattedWith: targetFormat, with: targetCalendarIdentifier, in: targetTimezone)
         }
         return nil
     }
     
     @available(iOS 8.0, *)
     mutating func reformatDate(formattedWith sourceFormat: String,
-                                      `in` sourceTimezone: TimeZone = TimeZone.current,
-                                      toStringFormattedWith targetFormat: String,
-                                      `in` targetTimezone: TimeZone = TimeZone.current) {
+                               `in` sourceTimezone: TimeZone = TimeZone.current,
+                               toStringFormattedWith targetFormat: String,
+                               `in` targetTimezone: TimeZone = TimeZone.current) {
         guard let date = Date(from: self, formattedWith: sourceFormat, in: sourceTimezone),
             let newStringDate = String(from: date, formattedWith: targetFormat, in: targetTimezone) else {
                 return
