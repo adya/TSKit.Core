0bdc514e6960b5e3227dbdadcc20d830dbf5d687
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2020-04-09 21:45:35 +0300

    WIP.

--- TSKit.Core/Sources/Extensions/Date/Date+Weekdays.swift
+++ TSKit.Core/Sources/Extensions/Date/Date+Weekdays.swift
@@ -20,28 +20,58 @@ public extension Date {
 
 public extension Date {
 
-    /// - Returns: Date for tomorrow at the exact same time.
-    func tomorrow(in calendar: Calendar = .current) -> Date {
-        return calendar.date(byAdding: .day, value: 1, to: self)!
+    enum Offset {
+        
+        case next(Int)
+        
+        case previous(Int)
+        
+        case current
+        
+        internal var value: Int {
+            switch self {
+            case .current: return 0
+            case .next(let value), .previous(let value): return value
+            }
+        }
     }
-
-    func week(_ offset: Int = 0, in calendar: Calendar = .current) -> DateInterval {
+    
+    func day(_ offset: Offset, in calendar: Calendar = .current) -> Date {
+        return calendar.date(byAdding: .day, value: offset.value, to: self)!
+    }
+    
+    func week(_ offset: Offset, in calendar: Calendar = .current) -> DateInterval {
         let currentWeekday = calendar.component(.weekday, from: self)
         let weekdays = calendar.range(of: .weekday, in: .weekOfYear, for: self)!
         let firstCurrentWeekdayDistance = -(currentWeekday - calendar.firstWeekday)
-        let firstOffsetWeekday = calendar.date(byAdding: .init(day: firstCurrentWeekdayDistance, weekOfYear: offset), to: self)!
+        let firstOffsetWeekday = calendar.date(byAdding: .init(day: firstCurrentWeekdayDistance, weekOfYear: offset.value), to: self)!
                                          .startOfDay(in: calendar)
         let lastOffsetWeekday = calendar.date(byAdding: .init(day: weekdays.count, second: -1), to: firstOffsetWeekday)!
         return .init(start: firstOffsetWeekday, end: lastOffsetWeekday)
     }
 
-    func month(_ offset: Int = 0, in calendar: Calendar = .current) -> DateInterval {
+    func month(_ offset: Offset, in calendar: Calendar = .current) -> DateInterval {
         let currentDay = calendar.component(.day, from: self)
         let firstCurrentDayDistance = -(currentDay - 1)
-        let firstOffsetDay = calendar.date(byAdding: .init(month: offset, day: firstCurrentDayDistance), to: self)!
+        let firstOffsetDay = calendar.date(byAdding: .init(month: offset.value, day: firstCurrentDayDistance), to: self)!
                                      .startOfDay(in: calendar)
         let lastOffsetDay = calendar.date(byAdding: .init(month: 1, second: -1), to: firstOffsetDay)!
         return .init(start: firstOffsetDay, end: lastOffsetDay)
+    }
+    
+//    func year(_ offset: Offset, in calendar: Calendar = .current) -> DateInterval {
+//        // TODO: Calculate year.
+//    }
+}
+
+public extension Date {
+    
+    func tomorrow(in calendar: Calendar = .current) -> Date {
+        return day(.next(1), in: calendar)
+    }
+    
+    func yesterday(in calendar: Calendar = .current) -> Date {
+        return day(.previous(1), in: calendar)
     }
 }
 
@@ -51,6 +81,11 @@ public struct DateInterval {
 
     private(set) public var end: Date
 
+    @available(iOS 10, *)
+    public var dateInterval: Foundation.DateInterval {
+        .init(start: start, end: end)
+    }
+    
     @available(iOS 10, *)
     public init(dateInterval: Foundation.DateInterval) {
         start = dateInterval.start
