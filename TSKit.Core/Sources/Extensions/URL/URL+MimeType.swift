d2ce85edb59d4921f88e52a4308bf877324448f1
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2020-06-29 15:09:43 +0300

    Added extension for URL to get mime type.

---
+++ TSKit.Core/Sources/Extensions/URL/URL+MimeType.swift
@@ -0,0 +1,14 @@
+import Foundation
+import MobileCoreServices
+
+public extension URL {
+    
+    /// Determines mime type of the file based on path extension.
+    var mimeType: String? {
+        if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, self.pathExtension as NSString, nil)?.takeRetainedValue(),
+           let mimetype = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType)?.takeRetainedValue() {
+            return mimetype as String
+        }
+        return nil
+    }
+}
